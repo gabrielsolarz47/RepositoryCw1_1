41f49e60c7c7a5144d08d572b0cd772be5f2a7d6
gabrielsolarz47 <gabinek03@gmail.com>
2025-10-07 09:33:26 +0000

    C1_1.1
    Zrobione zadanie 1

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-(function () {
-  //TODO
+(function() {
+  var ex1_button = document.getElementById('ex1_button');
+  var ex1_content = document.getElementById('ex1_content');
+
+  ex1_button.onclick = function() {
+    var tabela = [];
+    for (var i = 1; i < 10; i++) {
+      tabela.push(i);
+    }
+    ex1_content.innerHTML = tabela.toString();
+  }
 })();
+
