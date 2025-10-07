1d1e1d0f07c2444ba7fc25b59f1d231f25c6e6c0
gabrielsolarz47 <gabinek03@gmail.com>
2025-10-07 09:56:40 +0000

    C1.1.2 blad

--- script.js
+++ script.js
@@ -9,5 +9,27 @@
     }
     ex1_content.innerHTML = tabela.toString();
   }
+  var ex2_text = document.getElementById('ex2_text');
+  var ex2_content = document.getElementById('ex2_content');
+  ex2_text.onchange = function() {
+    if (ex2_text.value.length != 9) {
+      ex2_content.innerHTML = "Dlugosc numeru musi byc rowna 9";
+      const regex = /[^A-Za-z]/g;
+      if (regex.test(ex2_text.value)) {
+        ex2_content.innerHTML = "Numer telefonu nie moze zawierac liter";
+        const regex2 = /[!@#$%^&*()\-+={}[\]:;"'<>,.?\/|\\]/;
+        if (regex2.test(ex2_text.value)) {
+          ex2_content.innerHTML = "Numer telefonu nie moze zawierac znakow specjalnych";
+          if (ex2_text.value.length == 9) {
+            ex2_content.innerHTML = "Numer telefonu jest poprawny";
+          }
+        }
+      }
+    }
+    
+    
+    
+
+  }
 })();
 
